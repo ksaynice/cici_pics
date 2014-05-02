0863a23e9a29f23b085b3b518416d3a7f7756fb2
K <ksaynice@gmail.com>
2014-05-02 10:26:32 -0700

    search page ready

--- app/stylesheets/main_stylesheet.rb
+++ app/stylesheets/main_stylesheet.rb
@@ -6,7 +6,7 @@ class MainStylesheet < ApplicationStylesheet
   end
 
   def root_view(st)
-    st.background_color = color.white
+    st.background_color = color.light_gray
   end
 
   def hello_world(st)
@@ -18,4 +18,24 @@ class MainStylesheet < ApplicationStylesheet
     st.font = font.medium
   end
 
+  def search_label(st)
+    st.frame = {l: 10, t: 80, w: app_width - 20, h: 40}
+    st.text = 'Find'
+    st.color = color.white
+    st.font = font.large
+  end
+
+  def query(st)
+    st.frame = {l: 10, t: 130, w: app_width - 20, h: 50}
+    st.background_color = color.white
+    st.font = font.large
+  end
+
+  def submit_button(st)
+    st.frame = {l: 10, t: 190, w: app_width - 20, h: 50}
+    st.text = 'Go!'
+    st.color = color.white
+    st.background_color = color.blue
+    st.font = font.large
+  end
 end
